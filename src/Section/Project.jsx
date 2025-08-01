50b5da5f9e74be2b294540eab4180618d70c8ac2
Veritas 9908 <noeleruotor23@gmail.com>
2025-08-01 18:59:23 +0100

    update node modules

--- src/Section/Project.jsx
+++ src/Section/Project.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { Github } from "../assets";
-import { Projects } from "../Constants";
+import { Projects } from "../constants";
 
 gsap.registerPlugin(ScrollTrigger);
 
