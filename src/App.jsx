2ca12d6060d32583aaa9a4a6cb148a81ded6c959
Veritas 9908 <noeleruotor23@gmail.com>
2025-08-16 07:53:47 +0100

    lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
+import { Suspense, lazy } from "react";
 import StarsCanvas from "./assets/Canvas/Stars";
-import About from "./Section/About";
-import Contact from "./Section/Contact";
-import Footer from "./Section/Footer";
-import Header from "./Section/Header";
-import Navbar from "./Section/Navbar";
-import Project from "./Section/Project";
-import TechStack from "./Section/Tech";
+
+const About = lazy(() => import("./Section/About"));
+const Contact = lazy(() => import("./Section/Contact"));
+const Footer = lazy(() => import("./Section/Footer"));
+const Header = lazy(() => import("./Section/Header"));
+const Navbar = lazy(() => import("./Section/Navbar"));
+const Project = lazy(() => import("./Section/Project"));
+const TechStack = lazy(() => import("./Section/Tech"));
 
 function App() {
   return (
     <>
       <div className="relative mx-auto max-w-7xl duration-700">
-        <Navbar />
-        <Header />
-        <About />
-        <TechStack />
-        <Project />
-        <div className="relative">
-          <StarsCanvas />
-          <Contact />
-        </div>
-        <Footer />
+        <Suspense fallback={null}>
+          <Navbar />
+          <Header />
+          <About />
+          <TechStack />
+          <Project />
+          <div className="relative">
+            <StarsCanvas />
+            <Contact />
+          </div>
+          <Footer />
+        </Suspense>
       </div>
     </>
   );
